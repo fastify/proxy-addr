c9bfd9ce07d120bd3cb944661bfcd5c3da58f9a9
inyourtime <inyourtimeguy@gmail.com>
2025-08-04 21:19:18 +0700

    test: migrate to node:test

--- test/all.test.js
+++ test/all.test.js
@@ -1,55 +1,48 @@
 'use strict'
 
-const test = require('tape')
+const { test } = require('node:test')
 const proxyaddr = require('..')
 
 test('argument req should be required', function (t) {
-  t.throws(proxyaddr.all, /req.*required/u)
-  t.end()
+  t.assert.throws(proxyaddr.all, /req.*required/u)
 })
 
 test('argument trustshould be optional', function (t) {
   const req = createReq('127.0.0.1')
-  t.doesNotThrow(proxyaddr.all.bind(null, req))
-  t.end()
+  t.assert.doesNotThrow(proxyaddr.all.bind(null, req))
 })
 
 test('with no headers should return socket address', function (t) {
   const req = createReq('127.0.0.1')
-  t.same(proxyaddr.all(req), ['127.0.0.1'])
-  t.end()
+  t.assert.deepStrictEqual(proxyaddr.all(req), ['127.0.0.1'])
 })
 
 test('with x-forwarded-for header should include x-forwarded-for', function (t) {
   const req = createReq('127.0.0.1', {
     'x-forwarded-for': '10.0.0.1'
   })
-  t.same(proxyaddr.all(req), ['127.0.0.1', '10.0.0.1'])
-  t.end()
+  t.assert.deepStrictEqual(proxyaddr.all(req), ['127.0.0.1', '10.0.0.1'])
 })
 
 test('with x-forwarded-for header should include x-forwarded-for in correct order', function (t) {
   const req = createReq('127.0.0.1', {
     'x-forwarded-for': '10.0.0.1, 10.0.0.2'
   })
-  t.same(proxyaddr.all(req), ['127.0.0.1', '10.0.0.2', '10.0.0.1'])
-  t.end()
+  t.assert.deepStrictEqual(proxyaddr.all(req), ['127.0.0.1', '10.0.0.2', '10.0.0.1'])
 })
 
 test('with trust argument should stop at first untrusted', function (t) {
   const req = createReq('127.0.0.1', {
     'x-forwarded-for': '10.0.0.1, 10.0.0.2'
   })
-  t.same(proxyaddr.all(req, '127.0.0.1'), ['127.0.0.1', '10.0.0.2'])
-  t.end()
+  t.assert.deepStrictEqual(proxyaddr.all(req, '127.0.0.1'), ['127.0.0.1', '10.0.0.2'])
 })
 
 test('with trust argument should be only socket address for no trust', function (t) {
   const req = createReq('127.0.0.1', {
     'x-forwarded-for': '10.0.0.1, 10.0.0.2'
   })
-  t.same(proxyaddr.all(req, []), ['127.0.0.1'])
-  t.end()
+  t.assert.deepStrictEqual(proxyaddr.all(req, []), ['127.0.0.1'])
 })
 
 function createReq (socketAddr, headers) {
